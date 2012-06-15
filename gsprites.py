430a315fc5a7c4cfebed39e54fd73748f2c2cca0
Milan Barot <milanmbt@gmail.com>
2012-06-14 22:50:57 -0400

    New sprite and some minor semantic changes

--- gsprites.py
+++ gsprites.py
@@ -1,25 +1,30 @@
 import pygame, utils
 
 # Player character class
-class Protagonist(pygame.sprite.Sprite):
+class Protagonist(pygame.sprite.DirtySprite):
 	def __init__(self, imgPath, rect):
-		pygame.sprite.Sprite.__init__(self)
+		pygame.sprite.DirtySprite.__init__(self)
 		self.image = utils.loadImage(imgPath)
 		self.rect = rect
-		self.xVel, self.yVel = 0.0, 0.0
+		self.xVel, self.yVel = 0, 0
 		self.health = 100
 	def update(self):
+		if self.xVel == 0 and self.yVel == 0:
+			self.dirty = 0
+			return
+		self.dirty = 1
 		newX = self.rect[0] + self.xVel
 		newY = self.rect[1] + self.yVel
 		self.rect = (newX, newY, self.rect[2], self.rect[3])
-		self.xVel = self.yVel = 0.0
+		self.xVel = self.yVel = 0
 	def draw(self, surface):
-		surface.blit(self.image, self.rect)
+		if self.dirty == 1:
+			surface.blit(self.image, self.rect)
 
 # Enemy class
-class Enemy(pygame.sprite.Sprite):
+class Enemy(pygame.sprite.DirtySprite):
 	def __init__(self, imgPath, rect):
-		pygame.sprite.Sprite.__init__(self)
+		pygame.sprite.DirtySprite.__init__(self)
 		self.image = utils.loadImage(imgPath)
 		self.rect = rect
 		self.xVel, self.yVel = 0, 0
@@ -31,9 +36,9 @@ class Enemy(pygame.sprite.Sprite):
 		self.xVel = self.yVel = 0
 		
 # Brick obstacle class
-class Brick(pygame.sprite.Sprite):
+class Brick(pygame.sprite.DirtySprite):
 	def __init__(self, imgPath, rect):
-		pygame.sprite.Sprite.__init__(self)
+		pygame.sprite.DirtySprite.__init__(self)
 		self.image = utils.loadImage(imgPath)
 		self.rect = rect
 	def update(self):
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ dirtyRects = []
 # Create screen to draw on
 screen = pygame.display.set_mode((480,272))
 
-art = gsprites.Protagonist('articuno.gif', (0.0,0.0,32.0,32.0))
+art = gsprites.Protagonist('articuno.gif', (0,0,32,32))
 brick1 = gsprites.Brick('brick.png', (32,0,32,32))
 brick2 = gsprites.Brick('brick.png', (32,32,32,32))
 brick3 = gsprites.Brick('brick.png', (0,32,32,32))
@@ -23,7 +23,9 @@ bricks = pygame.sprite.Group(brick1, brick2, brick3)
 exit = False
 clock = pygame.time.Clock()
 
-# Main game loop
+##################
+# MAIN GAME LOOP #
+##################
 while(not exit):
 	# Event loop that checks for exit conditions
 	for event in pygame.event.get():
@@ -41,16 +43,16 @@ while(not exit):
 	if keyPressed[K_RIGHT]: art.xVel += 4
 	if keyPressed[K_LEFT]: art.xVel -= 4
 	
-	# Fills a black default background			
-	screen.fill((0, 0, 0))
+	# Fills a black default background in places where things moved	
+	if art.dirty == 1:	
+		screen.fill((0, 0, 0), art.rect)
 	
 	# dirtyRects: collection of rects where screen has changed
-	dirtyRects = [art.rect]
+	dirtyRects.append(art.rect)
 	
 	# gsprites.Protagonist's update sets all it's velocities to 0 after 
 	# incrementing its position with them
 	art.update()
-	
 	
 	dirtyRects.append(art.rect)
 	for b in bricks.sprites():
@@ -60,17 +62,15 @@ while(not exit):
 	art.draw(screen)
 	bricks.draw(screen)
 	
-	# Redraw the screen
+	# Redraw dirty parts of screen
 	pygame.display.update(dirtyRects)
 	
-	# ???
+	# Something said to do this; should work without it
 	pygame.event.pump()
 	
 	# Manage frame rate
 	clock.tick(24)
 	
-	# printed fps for debug purposes
-	# *** Should be changed to blitting fps counter on screen ***
-	# print clock.get_fps()
+	# Should show fps on screen using clock.get_fps()
 	
 	
