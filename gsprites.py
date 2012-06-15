f778d77b5f61ef8af87151375b50f41e9e972b9c
Milan Barot <milanmbt@gmail.com>
2012-06-14 20:38:33 -0400

    .gitignore added to ignore .pyc files and drawing is smoother and a lower resolution

--- gsprites.py
+++ gsprites.py
@@ -6,13 +6,13 @@ class Protagonist(pygame.sprite.Sprite):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = utils.loadImage(imgPath)
 		self.rect = rect
-		self.xVel, self.yVel = 0, 0
+		self.xVel, self.yVel = 0.0, 0.0
 		self.health = 100
 	def update(self):
 		newX = self.rect[0] + self.xVel
 		newY = self.rect[1] + self.yVel
 		self.rect = (newX, newY, self.rect[2], self.rect[3])
-		self.xVel = self.yVel = 0
+		self.xVel = self.yVel = 0.0
 	def draw(self, surface):
 		surface.blit(self.image, self.rect)
 
--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import pygame
 from pygame.locals import *
 import utils, gsprites
 
-
 # Initialize pygame and display module
 pygame.init()
 pygame.display.init()
 
+# Create empty dirtyRects list of rectangles for efficient drawing
 dirtyRects = []
-# Create screen to draw on
-screen = pygame.display.set_mode((1024,768))
 
-art = gsprites.Protagonist('articuno.gif', (0,0,32,32))
-brick1 = gsprites.Brick('brick.png', (32,0,0,0))
-brick2 = gsprites.Brick('brick.png', (32,32,0,0))
-brick3 = gsprites.Brick('brick.png', (0,32,0,0))
+# Create screen to draw on
+screen = pygame.display.set_mode((480,272))
+
+art = gsprites.Protagonist('articuno.gif', (0.0,0.0,32.0,32.0))
+brick1 = gsprites.Brick('brick.png', (32,0,32,32))
+brick2 = gsprites.Brick('brick.png', (32,32,32,32))
+brick3 = gsprites.Brick('brick.png', (0,32,32,32))
 
 bricks = pygame.sprite.Group(brick1, brick2, brick3)
 
@@ -28,18 +29,20 @@ while(not exit):
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			exit = True
+			break
 		elif event.type == KEYDOWN and event.key == K_ESCAPE:
 			exit = True
-	
+			break
+
 	# Key states for player movement
 	keyPressed = pygame.key.get_pressed()
-	if keyPressed[pygame.K_UP]: art.yVel -= 4
-	if keyPressed[pygame.K_DOWN]: art.yVel += 4
-	if keyPressed[pygame.K_RIGHT]: art.xVel += 4
-	if keyPressed[pygame.K_LEFT]: art.xVel -= 4
+	if keyPressed[K_UP]: art.yVel -= 4
+	if keyPressed[K_DOWN]: art.yVel += 4
+	if keyPressed[K_RIGHT]: art.xVel += 4
+	if keyPressed[K_LEFT]: art.xVel -= 4
 	
 	# Fills a black default background			
-	screen.fill(0x000000)
+	screen.fill((0, 0, 0))
 	
 	# dirtyRects: collection of rects where screen has changed
 	dirtyRects = [art.rect]
@@ -47,7 +50,12 @@ while(not exit):
 	# gsprites.Protagonist's update sets all it's velocities to 0 after 
 	# incrementing its position with them
 	art.update()
+	
+	
 	dirtyRects.append(art.rect)
+	for b in bricks.sprites():
+		dirtyRects.append(b.rect)
+	
 	# All drawing here
 	art.draw(screen)
 	bricks.draw(screen)
@@ -55,11 +63,14 @@ while(not exit):
 	# Redraw the screen
 	pygame.display.update(dirtyRects)
 	
+	# ???
+	pygame.event.pump()
+	
 	# Manage frame rate
-	clock.tick(60)
+	clock.tick(24)
 	
 	# printed fps for debug purposes
 	# *** Should be changed to blitting fps counter on screen ***
-	print clock.get_fps()
+	# print clock.get_fps()
 	
 	
