aabfece94ca7a9b8bb2d1512e4fffe18008c6a82
Milan Barot <milanmbt@gmail.com>
2012-06-16 11:07:33 -0400

    Added nice sprite sheets, project overhaul (sort of) imminent

--- gsprites.py
+++ gsprites.py
@@ -21,6 +21,7 @@ class Protagonist(pygame.sprite.DirtySprite):
 		if self.dirty == 1:
 			surface.blit(self.image, self.rect)
 
+'''
 # Enemy class
 class Enemy(pygame.sprite.DirtySprite):
 	def __init__(self, imgPath, rect):
@@ -34,9 +35,9 @@ class Enemy(pygame.sprite.DirtySprite):
 		newY = self.rect[1] + self.yVel
 		self.rect = (newX, newY, self.rect[2], self.rect[3])
 		self.xVel = self.yVel = 0
-		
-# Brick obstacle class
-class Brick(pygame.sprite.DirtySprite):
+'''		
+# Block obstacle class
+class Block(pygame.sprite.DirtySprite):
 	def __init__(self, imgPath, rect):
 		pygame.sprite.DirtySprite.__init__(self)
 		self.image = utils.loadImage(imgPath)
